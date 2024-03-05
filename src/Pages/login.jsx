84b367d26fa17cced5d213da11870142963a70a0
pinapin <a.iapin25@gmail.com>
2024-03-05 15:09:52 +0700

    add router

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -3,9 +3,11 @@ import AuthLayouts from "../components/Layouts/AuthLayouts";
 
 const LoginPage = () => {
   return (
-    <AuthLayouts title="Login">
-      <FormLogin />
-    </AuthLayouts>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <AuthLayouts title="Login">
+        <FormLogin />
+      </AuthLayouts>
+    </div>
   );
 };
 export default LoginPage;
--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -3,9 +3,11 @@ import AuthLayouts from "../components/Layouts/AuthLayouts";
 
 const RegisterPage = () => {
   return (
-    <AuthLayouts title="Register">
-      <FormRegister />
-    </AuthLayouts>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <AuthLayouts title="Register">
+        <FormRegister />
+      </AuthLayouts>
+    </div>
   );
 };
 export default RegisterPage;
